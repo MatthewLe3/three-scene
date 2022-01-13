d8e1d6ffaef8d99180b0bbfd975b8fb2247ffac4
taoli <15230817603@163.com>
2022-01-13 17:05:32 +0800

    路灯

--- src/views/ThreeScene.vue
+++ src/views/ThreeScene.vue
@@ -10,28 +10,26 @@
 	const gui = new dat.GUI();
 
 	var controls = new (function () {
-		this.color0 = [255,0,0];
-		this.color1 = [0,0,0];
-		this.color2 = [158,147,147];
-		this.color3 = [198,198,198];
-		this.color4 = [0,0,0];
-		this.color5 = [36,37,40];
-		this.color6 = [255,0,90];
+		this.color0 = [255, 0, 0];
+		this.color1 = [0, 0, 0];
+		this.color2 = [158, 147, 147];
+		this.color3 = [198, 198, 198];
+		this.color4 = [0, 0, 0];
+		this.color5 = [36, 37, 40];
+		this.color6 = [255, 0, 90];
 		this.color7 = [0, 128, 255];
 	})();
 
 	// 汽车的control
-	var carControl = gui.addFolder('跑车');
+	var carControl = gui.addFolder("跑车");
 
-	carControl.addColor(controls, "color0").name('尾翼');
-	carControl.addColor(controls, "color1").name('挡风玻璃');
-	carControl.addColor(controls, "color2").name('进气口');
-	carControl.addColor(controls, "color3").name('雨刷');
-	carControl.addColor(controls, "color4").name('车身');
-	carControl.addColor(controls, "color5").name('车胎');
-	carControl.open()
-	
-	
+	carControl.addColor(controls, "color0").name("尾翼");
+	carControl.addColor(controls, "color1").name("挡风玻璃");
+	carControl.addColor(controls, "color2").name("进气口");
+	carControl.addColor(controls, "color3").name("雨刷");
+	carControl.addColor(controls, "color4").name("车身");
+	carControl.addColor(controls, "color5").name("车胎");
+	carControl.open();
 
 	export default {
 		data() {
@@ -69,8 +67,10 @@
 				this.addCars();
 				// 添加房屋模型
 				this.addBuildingsModel();
-				// 添加动画
+				// 添加动画人物
 				this.addAnimation();
+				// 添加路灯
+				this.addLamtern();
 				// 渲染
 				this.render();
 
@@ -83,13 +83,32 @@
 			},
 			createLight() {
 				// 环境光
-				const ambientLight = new THREE.AmbientLight(0xffffff, .1);
+				const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
 				this.scene.add(ambientLight);
 				// 创建聚光灯
-				const spotLight = new THREE.SpotLight(0xffffff);
-				spotLight.position.set(0, 0, 3);
+				const spotLight = new THREE.SpotLight(0xffffff, 0.6);
+				// const pointLight = new THREE.PointLight(0xffffff, 1);
+				const targetObject = new THREE.Object3D();
+				spotLight.position.set(-8.4, -1, 2);
+				targetObject.position.set(-8.4, -1, 0);
+
+				this.scene.add(targetObject);
+				spotLight.target = targetObject;
 				spotLight.castShadow = true;
 				this.scene.add(spotLight);
+
+				const coneGeometry = new THREE.ConeGeometry(3.5, 2, 50);
+				
+				const coneMaterial = new THREE.MeshLambertMaterial({
+					color: 0xffff00,
+					transparent: true,
+					opacity: 0.4,
+					// depthTest: true,
+				});
+				const cone = new THREE.Mesh(coneGeometry, coneMaterial);
+				cone.position.set(-8.4, -1, 1.01);
+				cone.rotateX(Math.PI / 2);
+				this.scene.add(cone);
 			},
 			createCamera() {
 				this.camera = new THREE.PerspectiveCamera(
@@ -129,17 +148,26 @@
 
 				const geometry = new THREE.PlaneGeometry(20, 20, 1, 1);
 				let texture = THREE.ImageUtils.loadTexture("/image/ground.png");
+				let texture2 = THREE.ImageUtils.loadTexture(
+					"/models/lamtern/Fiberglass0014_S.jpg"
+				);
 				texture.wrapS = THREE.RepeatWrapping; //重复平铺
 				texture.wrapT = THREE.RepeatWrapping; //重复平铺
 				texture.repeat.set(4, 4); //重复次数
+
+				texture2.wrapS = THREE.RepeatWrapping; //重复平铺
+				texture2.wrapT = THREE.RepeatWrapping; //重复平铺
+				texture2.repeat.set(4, 4); //重复次数
 				// MeshBasicMaterial 不受光照影响
 				// MeshLambertMaterial 对光照有反应，用于创建暗淡的不发光的物体
 				// MeshPhongMaterial 对光照有反应，用于创建金属类明亮物体
-				const material = new THREE.MeshPhongMaterial ({
-					color:0xafc0ca,
+				const material = new THREE.MeshPhongMaterial({
+					color: 0xafc0ca,
 					map: texture,
+					bumpMap: texture2,
+					bumpScale: 0.03,
 					side: THREE.DoubleSide,
-					depthTest: true
+					depthTest: true,
 				});
 				const cube = new THREE.Mesh(geometry, material);
 				cube.receiveShadow = true;
@@ -154,6 +182,7 @@
 				});
 				let cubeMesh = new THREE.Mesh(cubeGeometry, cubeMat);
 				cubeMesh.position.set(-7.5, 7.5, 2.55); //设置立方体的坐标
+				cubeMesh.receiveShadow = true;
 				this.scene.add(cubeMesh);
 
 				let texture = THREE.ImageUtils.loadTexture(
@@ -167,6 +196,7 @@
 					map: texture,
 				});
 				let planeMesh = new THREE.Mesh(plane, planeMaterial);
+				planeMesh.receiveShadow = true;
 				planeMesh.position.set(-7.5, 5.99, 2.5);
 				planeMesh.rotateX(Math.PI / 2);
 				this.scene.add(planeMesh);
@@ -187,6 +217,12 @@
 							if (child.isMesh) {
 								child.castShadow = true;
 								child.receiveShadow = true;
+								if (child.children) {
+									child.children.forEach((c) => {
+										c.castShadow = true;
+										c.receiveShadow = true;
+									});
+								}
 							}
 						});
 					}
@@ -229,7 +265,7 @@
 						}
 					});
 
-					this.person.castShadow = true
+					this.person.castShadow = true;
 					this.person.scale.set(0.01, 0.01, 0.01);
 					this.person.position.set(-9, 0, 0.01);
 					this.person.rotateX(Math.PI / 2);
@@ -254,6 +290,40 @@
 					// 获取渲染的时间间隔
 					this.clock = new THREE.Clock();
 					this.scene.add(this.person);
+				});
+			},
+			addLamtern() {
+				let fbxLoader = new FBXLoader();
+				fbxLoader.load("/models/lamtern/streetlamp.fbx", (object) => {
+					this.lamtern = object;
+					let texture1 = THREE.ImageUtils.loadTexture(
+						"/models/lamtern/Fiberglass0014_S.jpg"
+					);
+					let texture2 = THREE.ImageUtils.loadTexture(
+						"/models/lamtern/MetalBase0121_9_S.jpg"
+					);
+					let texture3 = THREE.ImageUtils.loadTexture(
+						"/models/lamtern/MetalBase0123_S.jpg"
+					);
+
+					this.lamtern.traverse(function (child) {
+						if (child.isMesh) {
+							child.castShadow = true;
+							child.receiveShadow = true;
+							child.material.map = texture3;
+							child.material.lightMap = texture1;
+							console.log("child.material", child.material);
+						}
+					});
+					this.lamtern.receiveShadow = true;
+					this.lamtern.castShadow = true;
+					this.lamtern.scale.set(0.2, 0.2, 0.2);
+					this.lamtern.position.set(-8, -1, 0.01);
+					this.lamtern.rotateX(Math.PI / 2);
+
+					this.scene.add(this.lamtern);
+
+					console.log(this.lamtern);
 				});
 			},
 			render() {
@@ -314,11 +384,13 @@
 				this.controls.autoRotateSpeed = 1;
 
 				// OrbitControls
-				var orbitControl = gui.addFolder('OrbitControls');
-				orbitControl.add(this.controls,'autoRotate',[false,true]).onChange(e=>{
-					this.controls.autoRotate = JSON.parse(e)
-				})
-				orbitControl.open()
+				var orbitControl = gui.addFolder("OrbitControls");
+				orbitControl
+					.add(this.controls, "autoRotate", [false, true])
+					.onChange((e) => {
+						this.controls.autoRotate = JSON.parse(e);
+					});
+				orbitControl.open();
 			},
 		},
 	};
